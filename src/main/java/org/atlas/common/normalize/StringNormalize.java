9ff4f317f7afcf6c68a4623608f891f0d968bccd
Davi Fonseca Viana <daviviana2602@gmail.com>
2026-07-01 17:32:42 -0300

    feat: add StringNormalize (name and email)

--- src/main/java/org/atlas/common/normalize/StringNormalize.java
+++ src/main/java/org/atlas/common/normalize/StringNormalize.java
@@ -1,4 +1,15 @@
 package org.atlas.common.normalize;
 
 public class StringNormalize {
+
+
+    public static String normalizeName(String name) {
+        return name.trim().replaceAll("\\s+", " ");
+    }
+
+
+    public static String normalizeEmail(String email) {
+        return email.trim().toLowerCase();
+    }
+
 }
